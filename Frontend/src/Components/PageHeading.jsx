d43778c6903ba6be3f9029e8b1a2f3d67a879bd8
Nikicoder23 <nikithar2319@gmail.com>
2026-01-17 22:51:58 +0530

    added react events and hooks

--- Frontend/src/Components/PageHeading.jsx
+++ Frontend/src/Components/PageHeading.jsx
@@ -4,9 +4,9 @@ import PageTitle from './PageTitle';
 
 export default function PageHeading(props) {
   return (
-    <div className='page-heading-container'>
+    <div className='text-center max-w-[576px] mx-auto px-4 py-6'>
       <PageTitle title ="Explore eazy stickers"></PageTitle>
-      <p className='page-heading-paragraph'>{props.children}</p>
+      <p className='font-primary leading-6 text-gray-600'>{props.children}</p>
     </div>
   );
 }

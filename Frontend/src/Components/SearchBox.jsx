89db7d0632d59e681a7a291835f06ce74dc47009
Nikicoder23 <nikithar2319@gmail.com>
2026-01-23 19:06:54 +0530

    Added react hooks

--- Frontend/src/Components/SearchBox.jsx
+++ Frontend/src/Components/SearchBox.jsx
@@ -6,10 +6,10 @@ export default function SearchBox({label,placeholder,value,handlesSearch}) {
 
   return (
     <div className="flex items-center gap-3 pl-4 flex-1 font-primary">
-    <label className="text-lg font-semibold text-primary">{label}</label>
+    <label className="text-lg font-semibold text-primary dark:text-light">{label}</label>
      <input
         type="text"
-        className="px-4 py-2 text-base border rounded-md transition border-primary focus:ring focus:ring-dark focus:outline-none text-gray-800"
+        className="px-4 py-2 text-base border rounded-md transition border-primary focus:ring focus:ring-dark dark:focus:ring-lighter focus:outline-none text-gray-800 dark:text-light"
         placeholder={placeholder}
         value={value}
         onChange={(event)=>handlesSearch(event.target.value,event)}
--- Frontend/src/Components/footer/Footer.jsx
+++ Frontend/src/Components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer(){
     return(
-        <footer className="flex justify-center items-center py-4 font-primary text-gray-700">
+        <footer className="flex justify-center items-center py-4 font-primary text-gray-700 dark:text-light">
             Built with
             <FontAwesomeIcon icon={faHeart} className="text-red-600 mx-1 animate-pulse"
             aria-hidden="true"></FontAwesomeIcon>

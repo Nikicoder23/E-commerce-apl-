2622d1d2f152a3002b78fc48f4f11f506b8d81ae
Nikicoder23 <nikithar2319@gmail.com>
2026-01-24 21:56:54 +0530

    Created routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,12 @@ import Footer from "./Components/footer/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import React from "react";
+import { Outlet } from "react-router-dom";
 function App() {
  return(
   <React.Fragment>
   <Header/>
-  <Home></Home>
+  <Outlet/>
   <Footer/>
   </React.Fragment>
  );

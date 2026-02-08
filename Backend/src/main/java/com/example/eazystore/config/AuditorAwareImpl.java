dcd5eba64094296a629d943474804d7ae400c231
Nikicoder23 <nikithar2319@gmail.com>
2026-02-08 19:53:22 +0530

    Added exceptions,Scope and auditing

---
+++ Backend/src/main/java/com/example/eazystore/config/AuditorAwareImpl.java
@@ -0,0 +1,16 @@
+package com.example.eazystore.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+    @Component("auditorAwareImpl")
+    public class AuditorAwareImpl implements AuditorAware<String> {
+
+        @Override
+        public Optional<String> getCurrentAuditor() {
+            return Optional.of("Anonymous user");
+        }
+    }
+
--- Backend/src/main/java/com/example/eazystore/dto/ContactRequestDto.java
+++ Backend/src/main/java/com/example/eazystore/dto/ContactRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.eazystore.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +10,16 @@ import java.time.Instant;
 @Getter
 @Setter
 public class ContactRequestDto {
+
+    @NotBlank(message = "Name can not be empty")
     private String name;
+
+    @NotBlank(message = "Email can not be empty")
     private String email;
+
+    @NotBlank(message = "Mobile Number can not be empty")
     private String mobileNumber;
+
+    @NotBlank(message = "Message can not be empty")
     private String message;
 }
